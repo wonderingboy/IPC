ce60621fd04c2fe0872724e7e0967d2d0112631d
ChaoHe <1169731259@qq.com>
2018-04-19 12:15:02 +0800

    signal

--- p1/main.cpp
+++ p1/main.cpp
@@ -1,23 +1,86 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+#include <sys/sem.h>
 #include <sys/stat.h>
 #include <iostream>
 using namespace std;
-#define PERM IPC_EXCL|IPC_CREAT
+#define PERM IPC_CREAT
+union semun{
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+};
+// initialize 
+int set_semvalue(int sem_id){
+	union semun sem_union;
+	sem_union.val=1;
+	if(semctl(sem_id, 0, SETVAL, sem_union) == -1)
+    {
+        return 0;
+    }
+    return 1;
+}	
+// substract one occupy release
+static int semaphore_p(int sem_id) {
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = -1;//P()
+    sem_b.sem_flg = SEM_UNDO;
+    if(semop(sem_id, &sem_b, 1) == -1)
+    {
+        return 0;
+    }
+    return 1;
+}
+// add one release source
+static int semaphore_v(int sem_id)
+{
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = 1;//V()
+    sem_b.sem_flg = SEM_UNDO;
+    if(semop(sem_id, &sem_b, 1) == -1)
+    {
+        return 0;
+    }
+    return 1;
+}
+// delete sem
+static void del_semvalue(int sem_id)
+{
+    union semun sem_union;
+ 
+    if(semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
+    {
+    }
+}
+
+
 int main() {
 	int shmid=shmget(IPC_PRIVATE,1024,PERM|0666);
-	cout<<shmid<<endl;
+	if(-1==shmid){
+		cout<<"fail to allocate shm"<<endl;
+		return 0;
+	}
+	int semid=semget((key_t)3524,1,0666|PERM);
+	if (-1==semid) {
+		cout<<"fail to allocate sem"<<endl;
+		return 0;
+	}
+	cout<<"shmid: "<<shmid<<endl; 
+	cout<<"semid: "<<semid<<endl;
 	void * addr=shmat(shmid,0,SHM_RND);
 	char * op= (char *)addr ;
-	cout<<op<<endl;
-	for(int i=0;i<1024;i++) {
-		op[i]='i'; 
-		cout<<i;
+	if(-1== set_semvalue(semid)) {
+		cout<<"fail to initialize signal"<<endl;
+		return 0;
 	}
-	cout<<endl;
-	while (true) {
-		;
+	while(1) {
+		semaphore_p(semid);
+		op[1]='h';
+		op[1]=op[1]+1;
+		semaphore_v(semid);
 	}
 	return 0;
 }
--- p2/main.cpp
+++ p2/main.cpp
@@ -1,21 +1,72 @@
+#include<stdlib.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+#include <sys/sem.h>
 #include <sys/stat.h>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 using namespace std;
+#define PERM IPC_EXCL|IPC_CREAT
+union semun{
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+};
+// initialize 
+int set_semvalueint(int sem_id){
+	union semun sem_union;
+	sem_union.val=1;
+	if(semctl(sem_id, 0, SETVAL, sem_union) == -1)
+    {
+        return 0;
+    }
+    return 1;
+}	
+// substract one occupy release
+static int semaphore_p(int sem_id) {
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = -1;//P()
+    sem_b.sem_flg = SEM_UNDO;
+    if(semop(sem_id, &sem_b, 1) == -1)
+    {
+        return 0;
+    }
+    return 1;
+}
+// add one release source
+static int semaphore_v(int sem_id)
+{
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = 1;//V()
+    sem_b.sem_flg = SEM_UNDO;
+    if(semop(sem_id, &sem_b, 1) == -1)
+    {
+        return 0;
+    }
+    return 1;
+}
+// delete sem
+static void del_semvalue(int sem_id)
+{
+    union semun sem_union;
+ 
+    if(semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
+    {
+    }
+}
+
 int main(int argc,char* argv[]) {
-	int shid=atoi(argv[1]);
-	cout<<shid<<endl;
-	char *addr=(char*) shmat(shid,0,0);
-	for(int i=0;i<1024;i++) {
-		cout<<addr[i]<<" ";
+	int shmid=atoi(argv[1]);
+	int semid=atoi(argv[2]);
+	cout<<"shmid: "<<shmid<<endl;
+	cout<<"semid: "<<semid<<endl;
+	char *addr=(char*) shmat(shmid,0,0);
+	while(1) {
+//		semaphore_p(semid);
+		cout<<addr[1]<<endl;
+//		semaphore_v(semid);
 	} 
-	cout<<endl;
-	while(1){
-		;
-	}	
 	return 0;
 }
